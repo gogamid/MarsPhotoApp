bf4ea74341ec51ecef2cf5330cce36929d478a17
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-21 12:07:54 +0200

    fix: crash on network error

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -25,22 +25,27 @@ import kotlinx.coroutines.launch
 
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+  // The internal MutableLiveData that stores the status of the most recent request
+  private val _status = MutableLiveData<String>()
 
-    // The external immutable LiveData for the request status
-    val status: LiveData<String> = _status
-    /**
-     * Call getMarsPhotos() on init so we can display status immediately.
-     */
-    init {
-        getMarsPhotos()
-    }
+  // The external immutable LiveData for the request status
+  val status: LiveData<String> = _status
 
-    private fun getMarsPhotos() {
-      viewModelScope.launch {
-          val listResult = MarsApi.retrofitService.getPhotos()
-          _status.value = listResult
+  /**
+   * Call getMarsPhotos() on init so we can display status immediately.
+   */
+  init {
+    getMarsPhotos()
+  }
+
+  private fun getMarsPhotos() {
+    viewModelScope.launch {
+      try {
+        val listResult = MarsApi.retrofitService.getPhotos()
+        _status.value = listResult
+      } catch (e: Exception) {
+        _status.value = "Failure: ${e.message}"
       }
     }
+  }
 }
