3579d60f7a569ad2c7ab1a6cf608cd7945519a6f
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-23 18:18:34 +0200

    feat: display an internet image

--- app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.Json
 data class MarsPhoto(
   val id: String,
   @Json(name = "img_src")
-  val img_src: String
+  val imgSrcUrl: String
 )
--- app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
@@ -23,30 +23,31 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.android.marsphotos.databinding.FragmentOverviewBinding
+import com.example.android.marsphotos.databinding.GridViewItemBinding
 
 /**
  * This fragment shows the the status of the Mars photos web services transaction.
  */
 class OverviewFragment : Fragment() {
 
-    private val viewModel: OverviewViewModel by viewModels()
+  private val viewModel: OverviewViewModel by viewModels()
 
-    /**
-     * Inflates the layout with Data Binding, sets its lifecycle owner to the OverviewFragment
-     * to enable Data Binding to observe LiveData, and sets up the RecyclerView with an adapter.
-     */
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val binding = FragmentOverviewBinding.inflate(inflater)
+  /**
+   * Inflates the layout with Data Binding, sets its lifecycle owner to the OverviewFragment
+   * to enable Data Binding to observe LiveData, and sets up the RecyclerView with an adapter.
+   */
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    val binding = GridViewItemBinding.inflate(inflater)
 
-        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
-        binding.lifecycleOwner = this
+    // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+    binding.lifecycleOwner = this
 
-        // Giving the binding access to the OverviewViewModel
-        binding.viewModel = viewModel
+    // Giving the binding access to the OverviewViewModel
+    binding.viewModel = viewModel
 
-        return binding.root
-    }
+    return binding.root
+  }
 }
